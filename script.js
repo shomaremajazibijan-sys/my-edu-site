36b0f0f68cbde1b946d49c35bf731ada2686ce37
shomaremajazibijan-sys <shomare.majazibijan@gmail.com>
2025-12-12 17:06:02 +0330

    Add files via upload

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        let target = document.querySelector(link.getAttribute("href"));
-        target.scrollIntoView({ behavior: "smooth" });
-    });
-});
